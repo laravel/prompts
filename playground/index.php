2ad153dd3355a47387f31bce942437e595e90d94
Jess Archer <jess@jessarcher.com>
2023-03-31 15:59:55 +1000

    Update playground

--- playground/index.php
+++ playground/index.php
@@ -18,71 +18,74 @@ require __DIR__.'/../vendor/autoload.php';
 
 intro('Welcome to Laravel');
 
-$result = [
-    'name' => anticipate(
-        message: 'What is your name?',
-        placeholder: 'E.g. Taylor Otwell',
-        options: [
-            'Dries Vints',
-            'Guus Leeuw',
-            'James Brooks',
-            'Jess Archer',
-            'Joe Dixon',
-            'Mior Muhammad Zaki Mior Khairuddin',
-            'Nuno Maduro',
-            'Taylor Otwell',
-            'Tim MacDonald',
-        ],
-        validate: fn ($value) => match (true) {
-            ! $value => 'Please enter your name.',
-            default => null,
-        },
-    ),
-    'path' => text(
-        message: 'Where should we create your project?',
-        placeholder: 'E.g. ./laravel',
-        validate: fn ($value) => match (true) {
-            ! $value => 'Please enter a path',
-            $value[0] !== '.' => 'Please enter a relative path',
-            default => null,
-        },
-    ),
-    'password' => password(
-        message: 'Provide a password',
-        validate: fn ($value) => match (true) {
-            ! $value => 'Please enter a password.',
-            strlen($value) < 5 => 'Password should have at least 5 characters.',
-            default => null,
-        },
-    ),
-    'type' => select(
-        message: 'Pick a project type',
-        default: 'ts',
-        options: [
-            'ts' => 'TypeScript',
-            'js' => 'JavaScript',
-        ],
-    ),
-    'tools' => multiselect(
-        message: 'Select additional tools.',
-        default: ['pint', 'eslint'],
-        options: [
-            'pint' => 'Pint',
-            'eslint' => 'ESLint',
-            'prettier' => 'Prettier',
-        ],
-        validate: function ($values) {
-            if (count($values) === 0) {
-                return 'Please select at least one tool.';
-            }
-        }
-    ),
-    'install' => confirm(
-        message: 'Install dependencies?',
-    ),
-];
+$name = anticipate(
+    message: 'What is your name?',
+    placeholder: 'E.g. Taylor Otwell',
+    options: [
+        'Dries Vints',
+        'Guus Leeuw',
+        'James Brooks',
+        'Jess Archer',
+        'Joe Dixon',
+        'Mior Muhammad Zaki Mior Khairuddin',
+        'Nuno Maduro',
+        'Taylor Otwell',
+        'Tim MacDonald',
+    ],
+    validate: fn ($value) => match (true) {
+        ! $value => 'Please enter your name.',
+        default => null,
+    },
+);
 
-if ($result['install']) {
+$path = text(
+    message: 'Where should we create your project?',
+    placeholder: 'E.g. ./laravel',
+    validate: fn ($value) => match (true) {
+        ! $value => 'Please enter a path',
+        $value[0] !== '.' => 'Please enter a relative path',
+        default => null,
+    },
+);
+
+$password = password(
+    message: 'Provide a password',
+    validate: fn ($value) => match (true) {
+        ! $value => 'Please enter a password.',
+        strlen($value) < 5 => 'Password should have at least 5 characters.',
+        default => null,
+    },
+);
+
+$type = select(
+    message: 'Pick a project type',
+    default: 'ts',
+    options: [
+        'ts' => 'TypeScript',
+        'js' => 'JavaScript',
+    ],
+);
+
+$tools = multiselect(
+    message: 'Select additional tools.',
+    default: ['pint', 'eslint'],
+    options: [
+        'pint' => 'Pint',
+        'eslint' => 'ESLint',
+        'prettier' => 'Prettier',
+    ],
+    validate: function ($values) {
+        if (count($values) === 0) {
+            return 'Please select at least one tool.';
+        }
+    }
+);
+
+$install = confirm(
+    message: 'Install dependencies?',
+);
+
+if ($install) {
     spin(fn () => sleep(3), 'Installing dependencies...');
 }
 
@@ -101,4 +104,4 @@ note(<<<EOT
 
 outro('Happy coding!');
 
-var_dump($result);
+var_dump(compact('name', 'path', 'password', 'type', 'tools', 'install'));
