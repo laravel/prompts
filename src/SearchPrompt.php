a109cc55fb9af365a06aff236184173e673785bc
Jess Archer <jess@jessarcher.com>
2023-08-11 11:24:19 +1000

    Docblock fixes

--- src/SearchPrompt.php
+++ src/SearchPrompt.php
@@ -22,7 +22,7 @@ class SearchPrompt extends Prompt
     protected ?array $matches = null;
 
     /**
-     * Create a new SuggestPrompt instance.
+     * Create a new SearchPrompt instance.
      *
      * @param  Closure(string): array<int|string, string>  $options
      */
@@ -44,6 +44,9 @@ class SearchPrompt extends Prompt
         });
     }
 
+    /**
+     * Perform the search.
+     */
     protected function search(): void
     {
         $this->state = 'searching';
@@ -115,11 +118,17 @@ class SearchPrompt extends Prompt
         }
     }
 
+    /**
+     * Get the current search query.
+     */
     public function searchValue(): string
     {
         return $this->typedValue;
     }
 
+    /**
+     * Get the selected value.
+     */
     public function value(): int|string|null
     {
         if ($this->matches === null || $this->highlighted === null) {
