8bdc73f6fdc177717b8e2eaf2df0e44da72d5721
Joe Tannenbaum <joe@joe.codes>
2023-10-21 15:18:33 -0400

    Update streaming-spinner.php

--- playground/streaming-spinner.php
+++ playground/streaming-spinner.php
@@ -6,11 +6,15 @@ use Symfony\Component\Process\Process;
 use function Laravel\Prompts\spin;
 use function Laravel\Prompts\text;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+if ($argv[1] ?? false) {
+    text('Name', 'Default');
+}
 
 spin(
     function (SpinnerMessenger $messenger) {
-        $process = Process::fromShellCommandline('php '.__DIR__.'/streaming-spinner-process.php');
+        $process = Process::fromShellCommandline('php ' . __DIR__ . '/streaming-spinner-process.php');
         $process->start();
 
         foreach ($process as $type => $data) {
@@ -22,27 +26,25 @@ spin(
     'Updating Composer...',
 );
 
-foreach (range(1, 3) as $i) {
-    if ($argv[1] ?? false) {
-        text('Name '.$i, 'Default '.$i);
-    }
-
-    spin(
-        function (SpinnerMessenger $messenger) {
-            foreach (range(1, 50) as $i) {
-                $messenger->line("<info>✔︎</info> <comment>Step {$i}</comment>");
-
-                usleep(rand(50_000, 250_000));
-
-                if ($i === 20) {
-                    $messenger->message('Almost there...');
-                }
-
-                if ($i === 35) {
-                    $messenger->message('Still going...');
-                }
-            }
-        },
-        'Taking necessary steps...',
-    );
+if ($argv[1] ?? false) {
+    text('Name ' . $i, 'Default ' . $i);
 }
+
+spin(
+    function (SpinnerMessenger $messenger) {
+        foreach (range(1, 50) as $i) {
+            $messenger->line("<info>✔︎</info> <comment>Step {$i}</comment>");
+
+            usleep(rand(50_000, 250_000));
+
+            if ($i === 20) {
+                $messenger->message('Almost there...');
+            }
+
+            if ($i === 35) {
+                $messenger->message('Still going...');
+            }
+        }
+    },
+    'Taking necessary steps...',
+);
