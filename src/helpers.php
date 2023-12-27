90eaad948195b57eabfa034c204e47307ff9e4c6
jessarcher <jessarcher@users.noreply.github.com>
2023-12-27 04:14:29 +0000

    Fix code styling

--- src/helpers.php
+++ src/helpers.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Collection;
 /**
  * Prompt the user for text input.
  */
-function text(string $label, string $placeholder = '', string $default = '', bool|string $required = false, Closure $validate = null, string $hint = ''): string
+function text(string $label, string $placeholder = '', string $default = '', bool|string $required = false, ?Closure $validate = null, string $hint = ''): string
 {
     return (new TextPrompt($label, $placeholder, $default, $required, $validate, $hint))->prompt();
 }
@@ -16,7 +16,7 @@ function text(string $label, string $placeholder = '', string $default = '', boo
 /**
  * Prompt the user for input, hiding the value.
  */
-function password(string $label, string $placeholder = '', bool|string $required = false, Closure $validate = null, string $hint = ''): string
+function password(string $label, string $placeholder = '', bool|string $required = false, ?Closure $validate = null, string $hint = ''): string
 {
     return (new PasswordPrompt($label, $placeholder, $required, $validate, $hint))->prompt();
 }
@@ -27,7 +27,7 @@ function password(string $label, string $placeholder = '', bool|string $required
  * @param  array<int|string, string>|Collection<int|string, string>  $options
  * @param  true|string  $required
  */
-function select(string $label, array|Collection $options, int|string $default = null, int $scroll = 5, Closure $validate = null, string $hint = '', bool|string $required = true): int|string
+function select(string $label, array|Collection $options, int|string|null $default = null, int $scroll = 5, ?Closure $validate = null, string $hint = '', bool|string $required = true): int|string
 {
     return (new SelectPrompt($label, $options, $default, $scroll, $validate, $hint, $required))->prompt();
 }
@@ -39,7 +39,7 @@ function select(string $label, array|Collection $options, int|string $default = 
  * @param  array<int|string>|Collection<int, int|string>  $default
  * @return array<int|string>
  */
-function multiselect(string $label, array|Collection $options, array|Collection $default = [], int $scroll = 5, bool|string $required = false, Closure $validate = null, string $hint = 'Use the space bar to select options.'): array
+function multiselect(string $label, array|Collection $options, array|Collection $default = [], int $scroll = 5, bool|string $required = false, ?Closure $validate = null, string $hint = 'Use the space bar to select options.'): array
 {
     return (new MultiSelectPrompt($label, $options, $default, $scroll, $required, $validate, $hint))->prompt();
 }
@@ -47,7 +47,7 @@ function multiselect(string $label, array|Collection $options, array|Collection 
 /**
  * Prompt the user to confirm an action.
  */
-function confirm(string $label, bool $default = true, string $yes = 'Yes', string $no = 'No', bool|string $required = false, Closure $validate = null, string $hint = ''): bool
+function confirm(string $label, bool $default = true, string $yes = 'Yes', string $no = 'No', bool|string $required = false, ?Closure $validate = null, string $hint = ''): bool
 {
     return (new ConfirmPrompt($label, $default, $yes, $no, $required, $validate, $hint))->prompt();
 }
@@ -57,7 +57,7 @@ function confirm(string $label, bool $default = true, string $yes = 'Yes', strin
  *
  * @param  array<string>|Collection<int, string>|Closure(string): array<string>  $options
  */
-function suggest(string $label, array|Collection|Closure $options, string $placeholder = '', string $default = '', int $scroll = 5, bool|string $required = false, Closure $validate = null, string $hint = ''): string
+function suggest(string $label, array|Collection|Closure $options, string $placeholder = '', string $default = '', int $scroll = 5, bool|string $required = false, ?Closure $validate = null, string $hint = ''): string
 {
     return (new SuggestPrompt($label, $options, $placeholder, $default, $scroll, $required, $validate, $hint))->prompt();
 }
@@ -68,7 +68,7 @@ function suggest(string $label, array|Collection|Closure $options, string $place
  * @param  Closure(string): array<int|string, string>  $options
  * @param  true|string  $required
  */
-function search(string $label, Closure $options, string $placeholder = '', int $scroll = 5, Closure $validate = null, string $hint = '', bool|string $required = true): int|string
+function search(string $label, Closure $options, string $placeholder = '', int $scroll = 5, ?Closure $validate = null, string $hint = '', bool|string $required = true): int|string
 {
     return (new SearchPrompt($label, $options, $placeholder, $scroll, $validate, $hint, $required))->prompt();
 }
@@ -79,7 +79,7 @@ function search(string $label, Closure $options, string $placeholder = '', int $
  * @param  Closure(string): array<int|string, string>  $options
  * @return array<int|string>
  */
-function multisearch(string $label, Closure $options, string $placeholder = '', int $scroll = 5, bool|string $required = false, Closure $validate = null, string $hint = 'Use the space bar to select options.'): array
+function multisearch(string $label, Closure $options, string $placeholder = '', int $scroll = 5, bool|string $required = false, ?Closure $validate = null, string $hint = 'Use the space bar to select options.'): array
 {
     return (new MultiSearchPrompt($label, $options, $placeholder, $scroll, $required, $validate, $hint))->prompt();
 }
@@ -100,7 +100,7 @@ function spin(Closure $callback, string $message = ''): mixed
 /**
  * Display a note.
  */
-function note(string $message, string $type = null): void
+function note(string $message, ?string $type = null): void
 {
     (new Note($message, $type))->display();
 }
@@ -159,7 +159,7 @@ function outro(string $message): void
  * @param  array<int, string|array<int, string>>|Collection<int, string|array<int, string>>  $headers
  * @param  array<int, array<int, string>>|Collection<int, array<int, string>>  $rows
  */
-function table(array|Collection $headers = [], array|Collection $rows = null): void
+function table(array|Collection $headers = [], array|Collection|null $rows = null): void
 {
     (new Table($headers, $rows))->display();
 }
@@ -174,7 +174,7 @@ function table(array|Collection $headers = [], array|Collection $rows = null): v
  * @param  ?Closure((TSteps is int ? int : value-of<TSteps>), Progress<TSteps>): TReturn  $callback
  * @return ($callback is null ? Progress<TSteps> : array<TReturn>)
  */
-function progress(string $label, iterable|int $steps, Closure $callback = null, string $hint = ''): array|Progress
+function progress(string $label, iterable|int $steps, ?Closure $callback = null, string $hint = ''): array|Progress
 {
     $progress = new Progress($label, $steps, $hint);
 
