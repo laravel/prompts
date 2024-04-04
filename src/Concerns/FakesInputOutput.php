3b5e6b03f1f1175574b5a32331d99c9819da9848
Jess Archer <jess@jessarcher.com>
2024-04-05 03:41:50 +1000

    Remove terminal height requirement (#128)
    * Ensure a minimimum scroll height of 1
    
    * Allow the terminal dimensions to be recalculated
    
    * Remove exception for minimum terminal height
    
    * Add additional helper methods for moving the cursor
    
    * Only re-render within the visible terminal height

--- src/Concerns/FakesInputOutput.php
+++ src/Concerns/FakesInputOutput.php
@@ -27,6 +27,7 @@ trait FakesInputOutput
         $mock->shouldReceive('restoreTty')->byDefault();
         $mock->shouldReceive('cols')->byDefault()->andReturn(80);
         $mock->shouldReceive('lines')->byDefault()->andReturn(24);
+        $mock->shouldReceive('initDimensions')->byDefault();
 
         foreach ($keys as $key) {
             $mock->shouldReceive('read')->once()->andReturn($key);
