86979ec320b7d509084e8add27e2b07594dffbb2
Len Woodward <Len@ProjektGopher.com>
2024-04-14 01:17:13 -0700

    add helper for tabbedscrollableselect

--- src/helpers.php
+++ src/helpers.php
@@ -53,6 +53,19 @@ function multiselect(string $label, array|Collection $options, array|Collection 
 }
 
 /**
+ * Prompt the user to select an option from a scrollable tabbed list.
+ * 
+ * @template TOption of array{id: int|string, tab: string, body: string}
+ * 
+ * @param  array<int, TOption>|Collection<int, TOption>  $options
+ * @param  int|Closure(Collection<int, TOption>): Collection<int, TOption> $default The default value for the prompt. If Closure, it is passed `$options` and should return a Collection containing only the desired record.
+ */
+function tabbedscrollableselect(string $label, array|Collection $options, int|Closure $default = 0, int $scroll = 14, int $max_width = 120, bool|string $required = true, mixed $validate = null, string $hint = ''): int|string|null
+{
+    return (new TabbedScrollableSelectPrompt(...func_get_args()))->prompt();
+}
+
+/**
  * Prompt the user to confirm an action.
  */
 function confirm(string $label, bool $default = true, string $yes = 'Yes', string $no = 'No', bool|string $required = false, mixed $validate = null, string $hint = ''): bool
