14e61f5121287f13555952a9d85cae0afed6cd58
macocci7 <macocci7@yahoo.co.jp>
2024-05-29 00:29:18 +0900

    update playground

--- playground/fileselector.php
+++ playground/fileselector.php
@@ -8,7 +8,7 @@ $model = fileselector(
     label: 'Select a file to import.',
     placeholder: 'E.g. ./vendor/autoload.php',
     validate: fn (string $value) => match (true) {
-        !is_readable($value) === 0 => 'Cannot read the file.',
+        !is_readable($value) => 'Cannot read the file.',
         default => null,
     },
     hint: 'Input the file path.',
