8f8be0710ae240481766a3a6a884bfc2750ea561
Tarsísio Xavier <tarsisiox@gmail.com>
2024-09-06 14:57:11 -0300

    chore(clear): improving playground test.

--- playground/clear.php
+++ playground/clear.php
@@ -1,8 +1,11 @@
 <?php
 
 use function Laravel\Prompts\clear;
+use function Laravel\Prompts\info;
 use function Laravel\Prompts\note;
+use function Laravel\Prompts\outro;
 use function Laravel\Prompts\pause;
+use function Laravel\Prompts\table;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -11,4 +14,24 @@ pause('When you type [enter] the terminal will be clean.');
 
 clear();
 
-note('All clear!');
+table(
+    ['name', 'email'],
+    [
+        ['Random Name', 'mail@mail.com'],
+        ['Another Random Name', 'another.mail@mail.com'],
+        ['Some wird Name huh?', 'wierd@mail.com'],
+    ]
+);
+
+pause('Press [enter] again to clear the terminal.');
+
+clear();
+
+info('This is your secret key, copy it and keep it in somewhere safe.');
+note(md5(random_int(10, 1000)));
+
+pause('Press [enter] to continue');
+
+clear();
+
+outro('Bye! :)');
