9351069e6b6980bf498356466e01c71191b5ceb2
Jess Archer <jess@jessarcher.com>
2024-09-09 09:35:14 +1000

    Simplify playground example

--- playground/clear.php
+++ playground/clear.php
@@ -1,37 +1,19 @@
 <?php
 
 use function Laravel\Prompts\clear;
-use function Laravel\Prompts\info;
 use function Laravel\Prompts\note;
-use function Laravel\Prompts\outro;
 use function Laravel\Prompts\pause;
-use function Laravel\Prompts\table;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-note('Writing some text that will disapear.');
-pause('When you type [enter] the terminal will be clean.');
+note('This will disappear.');
+
+pause('Press [Enter] to continue.');
 
 clear();
 
-table(
-    ['name', 'email'],
-    [
-        ['Random Name', 'mail@mail.com'],
-        ['Another Random Name', 'another.mail@mail.com'],
-        ['Some wird Name huh?', 'wierd@mail.com'],
-    ]
-);
+note('This will also disappear.');
 
-pause('Press [enter] again to clear the terminal.');
+pause('Press [Enter] to continue.');
 
 clear();
-
-info('This is your secret key, copy it and keep it in somewhere safe.');
-note(md5(random_int(10, 1000)));
-
-pause('Press [enter] to continue');
-
-clear();
-
-outro('Bye! :)');
