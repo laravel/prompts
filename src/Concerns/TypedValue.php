86720e2f494b00ca44e5f2966359f0a39997e0da
Len Woodward <Len@ProjektGopher.com>
2024-09-17 16:58:16 -0700

    strengthen types

--- src/Concerns/TypedValue.php
+++ src/Concerns/TypedValue.php
@@ -27,7 +27,7 @@ trait TypedValue
             $this->cursorPosition = mb_strlen($this->typedValue);
         }
 
-        $this->on('key', function ($key) use ($submit, $ignore, $allowNewLine) {
+        $this->on('key', function (string $key) use ($submit, $ignore, $allowNewLine): void {
             if ($key !== '' &&
                 ($key[0] === "\e" || in_array($key, [Key::CTRL_B, Key::CTRL_F, Key::CTRL_A, Key::CTRL_E]))
             ) {
