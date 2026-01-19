c87e764871e628a6da4da647c39fb940add79add
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2026-01-19 15:14:27 +0000

    Fix code styling

--- tests/Feature/MultiSelectPromptTest.php
+++ tests/Feature/MultiSelectPromptTest.php
@@ -294,7 +294,7 @@ it('selects all options when a default is provided', function () {
 
 it('deselects all when all options are already default', function () {
     Prompt::fake([Key::CTRL_A, Key::ENTER]);
-    
+
     $result = multiselect(
         label: 'What are your favorite colors?',
         options: [
@@ -304,6 +304,6 @@ it('deselects all when all options are already default', function () {
         ],
         default: ['red', 'green', 'blue']
     );
-    
+
     expect($result)->toBe([]);
 });
